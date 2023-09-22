ef77cb2a202aa491e3a911dfea79c3ba1a6902ea
Theo <95878514+TheoW03@users.noreply.github.com>
2023-09-21 21:55:29 -0400

    set up server

---
+++ Server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const hostname = '127.0.0.2';
+const port = 3000;
+
+app.use(express.static(__dirname)); 
+
+app.get('', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
