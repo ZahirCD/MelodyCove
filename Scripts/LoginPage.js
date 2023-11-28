931c7a7dabab88bb5d44ffc6454175b519475d5b
Theo <95878514+TheoW03@users.noreply.github.com>
2023-11-28 18:23:49 -0500

    impl scripts for everything

--- Scripts/LoginPage.js
+++ Scripts/LoginPage.js
@@ -3,10 +3,10 @@ const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
+    // e.preventDefault();
     const email = loginForm.email.value;
     const password = loginForm.password.value;
-
+    console.log("test");
     if (email === "Admin@gmail.com" && password === "Admin") {
         alert("You have successfully logged in.");
         location.reload();
